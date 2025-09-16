77e6fd700011b718b199e2311cf4f78f0d1ffe7a
João Pedro Alves <jpafa13@gmail.com>
2025-09-16 15:35:36 -0300

    Segundo commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from views import *
+
+# ---------------- CONFIG ---------------- #
+DB_NAME = "usuarios.db"
+TRAIN_DIR = "fotos"
+MODEL_PATH = "modelo_lbph.yml"
+FACE_SIZE = (200, 200)
+
+
+# ---------------- MAIN ---------------- #
+if __name__ == "__main__":
+    init_db()
+    os.makedirs(TRAIN_DIR, exist_ok=True)
+    treinar_modelo()
+    root = tk.Tk(); app = App(root); root.mainloop()
